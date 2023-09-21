6673395d91e52167e5e5b0dc722ba0af15d8c8f0
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-09-21 05:07:53 +0300

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,7 +1,7 @@
 
 def generate_random_data():
-    random_string = 'Cell everything phone much government.'
-    random_number = 4
+    random_string = 'Deep small behavior month only probably culture.'
+    random_number = 60
 
     data = [(random_string, random_number) for _ in range(10)]
     return data
@@ -10,8 +10,8 @@ def main():
     data = generate_random_data()
     for item in data:
         random_string, random_number = item
-        print(f"Random String: Cell everything phone much government.")
-        print(f"Random Number: 4")
+        print(f"Random String: Deep small behavior month only probably culture.")
+        print(f"Random Number: 60")
 
 if __name__ == "__main__":
     main()
