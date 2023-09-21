e415ac922d192368280d6329491517dbf54a2a60
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-09-21 05:07:53 +0300

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,7 +1,7 @@
 
 def generate_random_data():
-    random_string = 'If six east religious chance foreign.'
-    random_number = 100
+    random_string = 'National professor too nice the food put.'
+    random_number = 13
 
     data = [(random_string, random_number) for _ in range(10)]
     return data
@@ -10,8 +10,8 @@ def main():
     data = generate_random_data()
     for item in data:
         random_string, random_number = item
-        print(f"Random String: If six east religious chance foreign.")
-        print(f"Random Number: 100")
+        print(f"Random String: National professor too nice the food put.")
+        print(f"Random Number: 13")
 
 if __name__ == "__main__":
     main()
